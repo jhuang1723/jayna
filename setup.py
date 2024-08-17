b016dd5ab16cd004fc7d5150b7d8f6dad39dd308
jhuang1723 <70125966+jhuang1723@users.noreply.github.com>
2024-08-16 23:33:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jayna",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         # List your package dependencies here
